940fed5f3941595a3c441ffcdfb965f7239e8dc8
Sreeharsha Raveendra <dev.sreeharsha@gmail.com>
2023-10-11 19:10:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 2000
+    };
+    return member;
+}
